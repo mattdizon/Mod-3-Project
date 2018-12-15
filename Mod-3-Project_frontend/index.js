289b443d00a00f346cd8fbc2a1bc9b166cc4c985
github username <github email address>
2018-12-15 15:17:37 -0500

    remove particles more mobile friendly

--- Mod-3-Project_frontend/index.js
+++ Mod-3-Project_frontend/index.js
@@ -521,177 +521,177 @@ let yellowCircleFunc = function yellowCircle(){
     }
 
 
-    //fireworks particle
-          anime.timeline({loop: true})
-            .add({
-              targets: '.ml8 .circle-white',
-              scale: [0, 3],
-              opacity: [1, 0],
-              easing: "easeInOutExpo",
-              rotateZ: 360,
-              duration: 1100
-            }).add({
-              targets: '.ml8 .circle-container',
-              scale: [0, 1],
-              duration: 1100,
-              easing: "easeInOutExpo",
-              offset: '-=1000'
-            }).add({
-              targets: '.ml8 .circle-dark',
-              scale: [0, 1],
-              duration: 1100,
-              easing: "easeOutExpo",
-              offset: '-=600'
-            }).add({
-              targets: '.ml8 .letters-left',
-              scale: [0, 1],
-              duration: 1200,
-              offset: '-=550'
-            }).add({
-              targets: '.ml8 .bang',
-              scale: [0, 1],
-              rotateZ: [45, 15],
-              duration: 1200,
-              offset: '-=1000'
-            }).add({
-              targets: '.ml8',
-              opacity: 0,
-              duration: 1000,
-              easing: "easeOutExpo",
-              delay: 1400
-            });
-
-          anime({
-            targets: '.ml8 .circle-dark-dashed',
-            rotateZ: 360,
-            duration: 8000,
-            easing: "linear",
-            loop: true
-          });
-
-    // function to add particles(fireworks)----------------------
-    window.human = false;
-
-    var ctx = htmlCanvas.getContext('2d');
-    var numberOfParticules = 30;
-    var pointerX = 0;
-    var pointerY = 0;
-    var tap = ('ontouchstart' in window || navigator.msMaxTouchPoints) ? 'touchstart' : 'mousedown';
-    var colors = ['#FF1461', '#18FF92', '#5A87FF', '#FBF38C'];
-
-    function setCanvasSize() {
-      htmlCanvas.width = window.innerWidth * 2;
-      htmlCanvas.height = window.innerHeight * 2;
-      htmlCanvas.style.width = window.innerWidth + 'px';
-      htmlCanvas.style.height = window.innerHeight + 'px';
-      htmlCanvas.getContext('2d').scale(2, 2);
-    }
-
-    function updateCoords(e) {
-      pointerX = e.clientX || e.touches[0].clientX;
-      pointerY = e.clientY || e.touches[0].clientY;
-    }
-
-    function setParticuleDirection(p) {
-      var angle = anime.random(0, 360) * Math.PI / 180;
-      var value = anime.random(50, 180);
-      var radius = [-1, 1][anime.random(0, 1)] * value;
-      return {
-        x: p.x + radius * Math.cos(angle),
-        y: p.y + radius * Math.sin(angle)
-      }
-    }
-
-    function createParticule(x,y) {
-      var p = {};
-      p.x = x;
-      p.y = y;
-      p.color = colors[anime.random(0, colors.length - 1)];
-      p.radius = anime.random(16, 32);
-      p.endPos = setParticuleDirection(p);
-      p.draw = function() {
-        ctx.beginPath();
-        ctx.arc(p.x, p.y, p.radius, 0, 2 * Math.PI, true);
-        ctx.fillStyle = p.color;
-        ctx.fill();
-      }
-      return p;
-    }
-
-    function createCircle(x,y) {
-      var p = {};
-      p.x = x;
-      p.y = y;
-      p.color = '#FFF';
-      p.radius = 0.1;
-      p.alpha = .5;
-      p.lineWidth = 6;
-      p.draw = function() {
-        ctx.globalAlpha = p.alpha;
-        ctx.beginPath();
-        ctx.arc(p.x, p.y, p.radius, 0, 2 * Math.PI, true);
-        ctx.lineWidth = p.lineWidth;
-        ctx.strokeStyle = p.color;
-        ctx.stroke();
-        ctx.globalAlpha = 1;
-      }
-      return p;
-    }
-
-    function renderParticule(anim) {
-      for (var i = 0; i < anim.animatables.length; i++) {
-        anim.animatables[i].target.draw();
-      }
-    }
-
-    function animateParticules(x, y) {
-      var circle = createCircle(x, y);
-      var particules = [];
-      for (var i = 0; i < numberOfParticules; i++) {
-        particules.push(createParticule(x, y));
-      }
-      anime.timeline().add({
-        targets: particules,
-        x: function(p) { return p.endPos.x; },
-        y: function(p) { return p.endPos.y; },
-        radius: 0.1,
-        duration: anime.random(1200, 1800),
-        easing: 'easeOutExpo',
-        update: renderParticule
-      })
-        .add({
-        targets: circle,
-        radius: anime.random(80, 160),
-        lineWidth: 0,
-        alpha: {
-          value: 0,
-          easing: 'linear',
-          duration: anime.random(600, 800),
-        },
-        duration: anime.random(1200, 1800),
-        easing: 'easeOutExpo',
-        update: renderParticule,
-        offset: 0
-      });
-    }
-
-    var render = anime({
-      duration: Infinity,
-      update: function() {
-        ctx.clearRect(pointerX-150, pointerY-150, 300, 300);
-      }
-    });
-
-    document.addEventListener(tap, function(e) {
-      window.human = true;
-      updateCoords(e);
-
-      animateParticules(pointerX, pointerY);
-      render.play();
-    }, false);
-
-    var centerX = window.innerWidth / 2;
-    var centerY = window.innerHeight / 2;
+    // //fireworks particle
+    //       anime.timeline({loop: true})
+    //         .add({
+    //           targets: '.ml8 .circle-white',
+    //           scale: [0, 3],
+    //           opacity: [1, 0],
+    //           easing: "easeInOutExpo",
+    //           rotateZ: 360,
+    //           duration: 1100
+    //         }).add({
+    //           targets: '.ml8 .circle-container',
+    //           scale: [0, 1],
+    //           duration: 1100,
+    //           easing: "easeInOutExpo",
+    //           offset: '-=1000'
+    //         }).add({
+    //           targets: '.ml8 .circle-dark',
+    //           scale: [0, 1],
+    //           duration: 1100,
+    //           easing: "easeOutExpo",
+    //           offset: '-=600'
+    //         }).add({
+    //           targets: '.ml8 .letters-left',
+    //           scale: [0, 1],
+    //           duration: 1200,
+    //           offset: '-=550'
+    //         }).add({
+    //           targets: '.ml8 .bang',
+    //           scale: [0, 1],
+    //           rotateZ: [45, 15],
+    //           duration: 1200,
+    //           offset: '-=1000'
+    //         }).add({
+    //           targets: '.ml8',
+    //           opacity: 0,
+    //           duration: 1000,
+    //           easing: "easeOutExpo",
+    //           delay: 1400
+    //         });
+    //
+    //       anime({
+    //         targets: '.ml8 .circle-dark-dashed',
+    //         rotateZ: 360,
+    //         duration: 8000,
+    //         easing: "linear",
+    //         loop: true
+    //       });
+    //
+    // // function to add particles(fireworks)----------------------
+    // window.human = false;
+    //
+    // var ctx = htmlCanvas.getContext('2d');
+    // var numberOfParticules = 30;
+    // var pointerX = 0;
+    // var pointerY = 0;
+    // var tap = ('ontouchstart' in window || navigator.msMaxTouchPoints) ? 'touchstart' : 'mousedown';
+    // var colors = ['#FF1461', '#18FF92', '#5A87FF', '#FBF38C'];
+    //
+    // function setCanvasSize() {
+    //   htmlCanvas.width = window.innerWidth * 2;
+    //   htmlCanvas.height = window.innerHeight * 2;
+    //   htmlCanvas.style.width = window.innerWidth + 'px';
+    //   htmlCanvas.style.height = window.innerHeight + 'px';
+    //   htmlCanvas.getContext('2d').scale(2, 2);
+    // }
+    //
+    // function updateCoords(e) {
+    //   pointerX = e.clientX || e.touches[0].clientX;
+    //   pointerY = e.clientY || e.touches[0].clientY;
+    // }
+    //
+    // function setParticuleDirection(p) {
+    //   var angle = anime.random(0, 360) * Math.PI / 180;
+    //   var value = anime.random(50, 180);
+    //   var radius = [-1, 1][anime.random(0, 1)] * value;
+    //   return {
+    //     x: p.x + radius * Math.cos(angle),
+    //     y: p.y + radius * Math.sin(angle)
+    //   }
+    // }
+    //
+    // function createParticule(x,y) {
+    //   var p = {};
+    //   p.x = x;
+    //   p.y = y;
+    //   p.color = colors[anime.random(0, colors.length - 1)];
+    //   p.radius = anime.random(16, 32);
+    //   p.endPos = setParticuleDirection(p);
+    //   p.draw = function() {
+    //     ctx.beginPath();
+    //     ctx.arc(p.x, p.y, p.radius, 0, 2 * Math.PI, true);
+    //     ctx.fillStyle = p.color;
+    //     ctx.fill();
+    //   }
+    //   return p;
+    // }
+    //
+    // function createCircle(x,y) {
+    //   var p = {};
+    //   p.x = x;
+    //   p.y = y;
+    //   p.color = '#FFF';
+    //   p.radius = 0.1;
+    //   p.alpha = .5;
+    //   p.lineWidth = 6;
+    //   p.draw = function() {
+    //     ctx.globalAlpha = p.alpha;
+    //     ctx.beginPath();
+    //     ctx.arc(p.x, p.y, p.radius, 0, 2 * Math.PI, true);
+    //     ctx.lineWidth = p.lineWidth;
+    //     ctx.strokeStyle = p.color;
+    //     ctx.stroke();
+    //     ctx.globalAlpha = 1;
+    //   }
+    //   return p;
+    // }
+    //
+    // function renderParticule(anim) {
+    //   for (var i = 0; i < anim.animatables.length; i++) {
+    //     anim.animatables[i].target.draw();
+    //   }
+    // }
+    //
+    // function animateParticules(x, y) {
+    //   var circle = createCircle(x, y);
+    //   var particules = [];
+    //   for (var i = 0; i < numberOfParticules; i++) {
+    //     particules.push(createParticule(x, y));
+    //   }
+    //   anime.timeline().add({
+    //     targets: particules,
+    //     x: function(p) { return p.endPos.x; },
+    //     y: function(p) { return p.endPos.y; },
+    //     radius: 0.1,
+    //     duration: anime.random(1200, 1800),
+    //     easing: 'easeOutExpo',
+    //     update: renderParticule
+    //   })
+    //     .add({
+    //     targets: circle,
+    //     radius: anime.random(80, 160),
+    //     lineWidth: 0,
+    //     alpha: {
+    //       value: 0,
+    //       easing: 'linear',
+    //       duration: anime.random(600, 800),
+    //     },
+    //     duration: anime.random(1200, 1800),
+    //     easing: 'easeOutExpo',
+    //     update: renderParticule,
+    //     offset: 0
+    //   });
+    // }
+    //
+    // var render = anime({
+    //   duration: Infinity,
+    //   update: function() {
+    //     ctx.clearRect(pointerX-150, pointerY-150, 300, 300);
+    //   }
+    // });
+    //
+    // document.addEventListener(tap, function(e) {
+    //   window.human = true;
+    //   updateCoords(e);
+    //
+    //   animateParticules(pointerX, pointerY);
+    //   render.play();
+    // }, false);
+    //
+    // var centerX = window.innerWidth / 2;
+    // var centerY = window.innerHeight / 2;
 
 
     resizeCanvas();
